717d5582b917511c9b4892f83475b4bda51cb484
Ritik Pal <rp-winter>
2023-01-18 22:43:04 +0530

    Adding comments in example.cpp

--- example.cpp
+++ example.cpp
@@ -8,17 +8,14 @@ using namespace std;
 int main() {
     float arr1x[] = {0.2, 0.25, 0.5, 1, 2, 3, 4};
     float arr1y[] = {0.04, 0.125, 0.25, 1, 4, 9, 16};
-    Gnuplot a("plot");
-    a.fplot("data.dat");
-    a.arrPlot2d(arr1x, arr1y);
-    a.w = "l";
-    a.fplot("data.dat", 1, 3);
-    a.dt = 3;
-    a.yplot("cos(x)");
-    a.t = "x^2 + 1";
-    float arr2x[] = {0.2, 0.5, 1, 2, 3, 4};
-    float arr2y[] = {1.04, 1.25, 2, 5, 10, 17};
-    a.arrPlot2d(arr2x, arr2y);
-    a.display();
+    Gnuplot a("plot"); // Creating object "a" for 2d plot using "plot"
+    a.fplot("data.dat"); // Plotting data stored in file with column 1 as x values and column 2 as y values
+    a.arrPlot2d(arr1x, arr1y); // Plotting Array
+    a.w = "l"; // Changing style of plot to line plot
+    a.fplot("data.dat", 2, 3); // Plotting data stored in file with column 2 as values and column 3 as y values
+    a.dt = 3; // Changing the line type to dashed line
+    a.yplot("cos(x)"); // Plotting y = cos(x)
+    a.t = "x^2 + 1"; // Chaning the Legend title of the plot
+    a.display(); // Displaying the plot
     return 0;
 }
